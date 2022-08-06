8a8e99084a06c4661d5e7431253a98c346023eab
anthonybchung <11941@coderacademy.edu.au>
2022-08-06 22:20:06 +1000

    two_sum test passe

--- two_sum/spec/two_sum_spec.rb
+++ two_sum/spec/two_sum_spec.rb
@@ -9,13 +9,13 @@ RSpec.describe 'two sum' do
     expect(index_start(3,1,[3,2,3])).to(eq(2))
   end
 
-  # it 'should return 2 index position' do
-  #   expect(two_sum([2,7,11,15],9)).to(eq([0,1]))
-  # end
+  it 'should return 2 index position' do
+    expect(two_sum([2,7,11,15],9)).to(eq([0,1]))
+  end
 
-  # it 'should return 2 index position example 2' do 
-  #   expect(two_sum([3,2,4],6)).to(eq([1,2]))
-  # end
+  it 'should return 2 index position example 2' do 
+    expect(two_sum([3,2,4],6)).to(eq([1,2]))
+  end
 
   it 'should return index position example 3' do
     expect(two_sum([3,2,3],6)).to(eq([0,2]))
