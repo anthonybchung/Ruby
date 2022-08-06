31f9fee047b94b089b333272f029d2fd921be4b3
anthonybchung <11941@coderacademy.edu.au>
2022-08-07 06:50:00 +1000

    palindrome number

---
+++ palindrome_number/palindrome.rb
@@ -0,0 +1,48 @@
+def second_index(arr)
+  arr_length = arr.length()
+  if(arr_length%2 == 0)
+    start_index = arr_length/2
+  else
+    start_index = arr_length/2.to_i + 1
+  end
+  return start_index
+end
+
+
+def second_array(arr)
+  start_second = second_index(arr)
+  return arr[start_second..-1]
+end
+
+def first_array(arr)
+  arr_length = arr.length()
+  start_second = second_index(arr)
+  if(arr_length%2 == 0)
+    finish_first = start_second - 1
+  else
+    finish_first = start_second - 2
+  end
+  return arr[0..finish_first]
+end
+
+# returns true if first sub == second sub reverse
+def is_two_sub_equal(sub1,sub2)
+  str1 = sub1.join("")
+  str2 = sub2.reverse.join("")
+
+  if(str1 == str2)
+    ans = true
+  else
+    ans = false
+  end
+
+  return ans
+end
+
+def is_palindrome(x)
+    arr = x.to_s.split("")
+    first = first_array(arr)
+    second = second_array(arr)
+    return is_two_sub_equal(first,second)
+end
+
---
+++ palindrome_number/spec/palindrome_spec.rb
@@ -0,0 +1,47 @@
+require_relative '../palindrome.rb'
+
+RSpec.describe 'array' do
+  it 'find the starting index of the sub array when array length is odd' do
+    expect(second_index([1,2,3,4,5])).to eq(3)
+  end
+
+  it 'finds the start index of the sub array when array length is even' do
+    expect(second_index([1,2,3,4,5,6])).to eq(3)
+  end
+
+  it 'returns sub array of the second part of the array, odd length' do
+    expect(second_array([1,2,3,4,5])).to eq([4,5])
+  end
+
+  it 'return sub array of the second part of the array, even length' do
+    expect(second_array([1,2,3,4,5,6])).to eq([4,5,6])
+  end
+
+  it 'return sub array of the first part of the array, odd length' do
+    expect(first_array([1,2,3,4,5])).to eq([1,2])
+  end
+
+  it 'return sub array of the first part of the array, even length' do
+    expect(first_array([1,2,3,4,5,6])).to eq([1,2,3])
+  end
+
+  it 'will return true two arrays are equal' do 
+    expect(is_two_sub_equal([1,2,3],[3,2,1])).to eq(true)
+  end
+
+  it 'will return false two arrays are not equal' do
+    expect(is_two_sub_equal([1,2,3],[3,2,5])).to eq(false)
+  end
+
+  it 'will return true if number is palindrome' do
+    expect(is_palindrome(121)).to eq(true)
+  end
+
+  it 'will return true if number is palindrome' do
+    expect(is_palindrome(4795974)).to eq(true)
+  end
+
+  it 'will return false if number is NOT palindrome' do
+    expect(is_palindrome(12345)).to eq(false)
+  end
+end
